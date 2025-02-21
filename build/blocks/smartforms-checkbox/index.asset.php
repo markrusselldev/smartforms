a1ad986549762ce2d7a19ea2e7280b66d0cc45a3
Mark Russell <mark@markrussell.io>
2025-02-21 16:39:26 -0700

    Refactor chat UI: integrate checkbox grid layout support for horizontal/vertical displays while preserving original styles.

--- build/blocks/smartforms-checkbox/index.asset.php
+++ build/blocks/smartforms-checkbox/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '38d49ab383df7bac4f00');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8b708de838dab61b8a6f');
