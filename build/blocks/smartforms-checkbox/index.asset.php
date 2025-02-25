68270e3fb3360b82fea04b831d8317859680308a
Mark Russell <mark@markrussell.io>
2025-02-25 10:26:59 -0700

    Before refactor to use gutenberg default display

--- build/blocks/smartforms-checkbox/index.asset.php
+++ build/blocks/smartforms-checkbox/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '526900dead3bd0482746');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1a4d6e9e9743ebb76761');
