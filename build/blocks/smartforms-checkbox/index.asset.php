cdaf058892b0661f1b7ca44bb412d8023595a6d9
Mark Russell <mark@markrussell.io>
2025-02-27 09:10:06 -0700

    Remove wp defaults from checkbox inputs that interfere with Bootstrap

--- build/blocks/smartforms-checkbox/index.asset.php
+++ build/blocks/smartforms-checkbox/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3a99734c80e6103c2591');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd85989ba49259f2672b4');
