9ae5c398e621496e24b6d6b63ec4387b42f6828d
Mark Russell <mark@markrussell.io>
2025-03-07 17:25:27 -0700

    fix(editor): update .sf-field-help margin for consistent help text display
    Set the margin for the .sf-field-help class to "10px 0 0 0 !important" in editor styles to override interfering rules and ensure consistent spacing.

--- build/blocks/smartforms-buttons/index.asset.php
+++ build/blocks/smartforms-buttons/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ca5d34a6c1901d15b436');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bc6e8ec9d5370ad8c3ee');
--- build/blocks/smartforms-checkbox/index.asset.php
+++ build/blocks/smartforms-checkbox/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '87057764324ee5a5b81d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e4d760cc74778a81f223');
