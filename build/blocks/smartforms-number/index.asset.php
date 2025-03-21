93ff6e8592f585937c398512f755a6f564cebec2
Mark Russell <mark@markrussell.io>
2025-03-20 23:31:19 -0600

    feat(number): add custom React icon (centered "123" SVG) in block index for consistency

--- build/blocks/smartforms-number/index.asset.php
+++ build/blocks/smartforms-number/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8ce02872721e345cf897');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '479c1453e465a699f06f');
--- src/blocks/smartforms-number/index.js
+++ src/blocks/smartforms-number/index.js
@@ -3,7 +3,25 @@ import edit from './edit';
 import './editor.scss';
 import './style.scss';
 
+// Define a custom icon as a React element for the Number block.
+const customIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <text
+      x="12"
+      y="16"
+      textAnchor="middle"
+      fontFamily="Arial, sans-serif"
+      fontSize="15"
+      fill="currentColor"
+      style={{ letterSpacing: '-1px' }}
+    >
+      123
+    </text>
+  </svg>
+);
+
 registerBlockType('smartforms/number', {
+  icon: customIcon,
   edit,
   save: () => null,
 });
